ab22c222fbe94172989645f6176a0d6c91c62236
Oleg <oleggavelya@gmail.com>
2026-04-09 11:21:30 +0300

    add table products database ShopDBReserve

--- create-database.sql
+++ create-database.sql
@@ -135,6 +135,12 @@ INSERT INTO Products (Name)
 CREATE DATABASE ShopDBReserve; 
 USE ShopDBReserve;
 
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
 CREATE DATABASE ShopDBDevelopment; 
 USE ShopDBDevelopment; 
 
